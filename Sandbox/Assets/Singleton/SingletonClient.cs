933218c8f44b5029c078b6103382f3610250517d
Tamás Losonczi <losonczi.tamas91@gmail.com>
2020-01-26 13:54:01 +0100

    [singleton] implement scene-scoped singleton

--- Sandbox/Assets/Singleton/SingletonClient.cs
+++ Sandbox/Assets/Singleton/SingletonClient.cs
@@ -1,4 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Singleton
 {
@@ -6,7 +10,35 @@ namespace Singleton
     {
         private void Awake()
         {
-            Singleton.Instance.Initialize();
+            StartCoroutine(UseSingleton());
+            StartCoroutine(ReloadScene());
+        }
+
+        private IEnumerator ReloadScene()
+        {
+            while (isActiveAndEnabled)
+            {
+                yield return new WaitForSeconds(5.0f);
+                SceneManager.LoadScene("SingletonScene");
+                ClearLog();
+            }
+        }
+        
+        private static void ClearLog()
+        {
+            var assembly = Assembly.GetAssembly(typeof(UnityEditor.Editor));
+            var type = assembly.GetType("UnityEditor.LogEntries");
+            var method = type.GetMethod("Clear");
+            method?.Invoke(new object(), null);
+        }
+        
+        private IEnumerator UseSingleton()
+        {
+            while (isActiveAndEnabled)
+            {
+                SingleSceneSingletonBehaviour.Instance.Increment();
+                yield return new WaitForSeconds(2.0f);
+            }
         }
     }
 }
