c2f4481206ff31d181621dccab49573288990347
Tamás Losonczi <losonczi.tamas91@gmail.com>
2022-09-13 18:15:52 +0200

    row shifting skeleton added

--- Sandbox/Assets/Tetris/Scripts/RowCleaner.cs
+++ Sandbox/Assets/Tetris/Scripts/RowCleaner.cs
@@ -4,30 +4,57 @@ namespace Tetris.Scripts
 {
     public class RowCleaner : MonoBehaviour
     {
+        private static readonly Vector3 ShiftOffset = Vector3.up;
+
+        private const int MaxRowCount = 20;
         private const int PieceCountPerRow = 10;
         
         [SerializeField] private Collider _collider;
 
         private readonly Collider[] _colliders = new Collider[16];
-        
+
+        private int _tetriminoLayer;
+
+        private void Awake()
+        {
+            _tetriminoLayer = LayerMask.GetMask("Tetrimino");
+        }
+
         private void OnGUI()
         {
             GUILayout.Space(100);
-            if (GUILayout.Button("Overlap test"))
+            if (GUILayout.Button("Clear level"))
             {
-                var bounds = _collider.bounds;
-                var tetriminoLayer = LayerMask.GetMask("Tetrimino");
-                var overlapCount = Physics.OverlapBoxNonAlloc(bounds.center, bounds.extents, _colliders, Quaternion.identity, tetriminoLayer);
-                if (overlapCount == PieceCountPerRow)
+                Clear();
+            }
+        }
+
+        private void Clear()
+        {
+            for (var i = 0; i < MaxRowCount; i++)
+            {
+                if (CheckRow(i))
                 {
-                    for (var colliderIndex = 0; colliderIndex < overlapCount; colliderIndex++)
-                    {
-                        var overlappingCollider = _colliders[colliderIndex];
-                        Destroy(overlappingCollider.gameObject);
-                    }  
-                    // TODO: check again and shift
+                    // TODO: move the pieces above
                 }
             }
+        }
+
+        private bool CheckRow(int rowIndex)
+        {
+            var bounds = _collider.bounds;
+            var center = bounds.center + rowIndex * ShiftOffset;
+            var overlapCount = Physics.OverlapBoxNonAlloc(center, bounds.extents, _colliders, Quaternion.identity, _tetriminoLayer);
+            var clearRow = overlapCount == PieceCountPerRow;
+            if (clearRow)
+            {
+                for (var colliderIndex = 0; colliderIndex < overlapCount; colliderIndex++)
+                {
+                    var overlappingCollider = _colliders[colliderIndex];
+                    Destroy(overlappingCollider.gameObject);
+                }  
+            }
+            return clearRow;
         }
     }
 }
