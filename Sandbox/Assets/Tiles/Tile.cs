2efefdd918bf20dda80459661c42d333b561dd96
Tamás Losonczi <losonczi.tamas91@gmail.com>
2022-10-09 21:55:02 +0200

    tile stuff added

---
+++ Sandbox/Assets/Tiles/Tile.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Tiles
+{
+    public class Tile : MonoBehaviour
+    {
+        public int RowIndex;
+        public int ColumnIndex;
+        
+        
+        
+    }
+}
